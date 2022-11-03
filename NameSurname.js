6e9b9ecdf6bca2d432808951826a27e97ce9d1fb
Вадим <93205098+LXXSE@users.noreply.github.com>
2022-11-03 16:22:21 +0700

    Add files via upload

---
+++ NameSurname.js
@@ -0,0 +1,26 @@
+let letName = "Тейп";
+let letSurname = "Носок";
+
+Name = letName.toLowerCase();
+Name1 = Name.substring(1);
+Name = Name.substring(0,1);
+Name = Name.toUpperCase();
+Name2 = Name + Name1
+
+Surname = letSurname.toLowerCase();
+Surname1 = Surname.substring(1);
+Surname = Surname.substring(0,1);
+Surname = Surname.toUpperCase();
+Surname2 = Surname + Surname1
+
+x = letName == Name2 && Surname2 == letSurname ? console.log("Имя и фамилия остались без изменений ") : console.log("Имя или фамилия преобразованы")
+
+
+//if (letName == Name2 && Surname2 == letSurname){
+//    console.log("Имя и фамилия остались без изменений ")
+//}
+//else{
+//    console.log("Имя или фамилия преобразованы")
+//}
+console.log(Name2 + " " + Surname2)
+
