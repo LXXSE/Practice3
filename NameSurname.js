00d1bb1871fd495d564fa4e8ef46bd2e58178e80
Вадим <93205098+lxxse@users.noreply.github.com>
2024-01-16 01:39:25 +0000

    last

---
+++ NameSurname.js
@@ -0,0 +1,29 @@
+let letName = "теЙп";
+let letSurname = "нОсОк";
+
+Name = letName.toLowerCase();
+Name1 = Name.substring(1);
+Name = Name.substring(0,1);
+Name = Name.toUpperCase();
+Name2 = Name + Name1
+
+Surname = letSurname.toLowerCase();
+Surname1 = Surname.substring(1);
+Surname = Surname.substring(0,1);
+Surname = Surname.toUpperCase();
+Surname2 = Surname + Surname1
+
+x = letName == Name2 && Surname2 == letSurname ? console.log("Имя и фамилия остались без изменений ") : console.log("Имя и/или фамилия преобразованы")
+
+
+//if (letName == Name2 && Surname2 == letSurname){
+//    console.log("Имя и фамилия остались без изменений ")
+//}
+//else{
+//    console.log("Имя или фамилия преобразованы")
+//}
+console.log(Name2 + " " + Surname2)
+
+// ??? mmr
+// ?? mmr2  
+// ???????? ALO
